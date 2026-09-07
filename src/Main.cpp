aea4fdd281178e74e1a7bd494b3a141fb3a77b6a
Pedro Segovia <157068737+Seggov@users.noreply.github.com>
2026-09-07 17:16:00 -0300

    Implement main function and menu for user input
    Added main function, menu, and input reading functionality.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,1 +1,56 @@
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+void main(){
+
+    lecturaInput();
+    menu();
+    selector();
+
+}
+
+int selector()
+{ 
+    int aux = 1; // por defecto visuliza el historial
+// llamar al input del usuario
+    return aux;
+
+
+}
+
+
+void menu()
+{
+    string opciones = "[1] Visualizar el Historial \n[2] Agregar Pacientes a la Fila";
+    cout << opciones << endl;
+
+}
+
+// 001; Emilio Vargas; 62; Cardiologia
+void lecturaInput()
+{
+    ifstream archivo("datos.txt");
+    string linea;
+
+    if(!archivo.is_open()){
+        cout << "no se puede abrir" << endl;
+    }
+
+    while (getline(archivo,linea)){
+
+        // le hacemos la linea con split de (";")
+        cout << linea << endln;
+        
+    }
+
+
+    archivo.close();
+    return 0;
+
+
+}
