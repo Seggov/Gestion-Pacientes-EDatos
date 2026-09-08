eaf68179de7716fd9de4f098a6a348b2ff4b2ffa
Pedro Segovia <157068737+Seggov@users.noreply.github.com>
2026-09-08 17:25:33 -0300

    Update Estructuras.cpp

--- src/Estructuras.cpp
+++ src/Estructuras.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+
+    
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
+    
+    Nodo(int valor) : dato(valor), siguiente(nullptr) {}
+}; // punto y coma necesarios para el sctruc 
